2e79ce216a9b94afc97c69c5f19743734d3eabfa
banup-kubeforce <102495939+banup-kubeforce@users.noreply.github.com>
2022-09-22 05:48:43 -0500

    Initial commit

---
+++ src/main/java/com/kubeforce/djlxray/XRAYFunction.java
@@ -0,0 +1,10 @@
+package com.example.djlxray;
+
+import java.util.function.Function;
+
+public class XRAYFunction implements Function {
+    @Override
+    public Object apply(Object o) {
+        return null;
+    }
+}
---
+++ src/test/java/com/kubeforce/djlxray/DjlxRayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.djlxray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjlxRayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
